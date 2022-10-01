07896e0664a463a0aef81fe06a5939e0b273f799
vphyre <vinipaulnic@hotmail.com>
2022-10-01 16:55:02 -0300

    Prism controller added

---
+++ Assets/Scripts/Gameplay/Colors/ColorEnum.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ColorEnum : uint
+{
+    Red = 0xFF0000,
+    Yellow = 0xFFFF00,
+    Blue = 0x0000FF,
+
+    Purple,
+    Orange,
+    Green,
+    
+}

89a4b4d81dcf1f64e1051785ce1bb9d4876c93aa
Cassio Moda <cassiomoda@gmail.com>
2022-10-02 16:03:27 -0300

    Creating Pause Menu

---
+++ Assets/Scripts/Ui/PauseMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public string mainMenuName = "MainMenu";
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Continue()
+    {
+        this.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(mainMenuName);
+    }
+}
